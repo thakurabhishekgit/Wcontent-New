ef6475f97ff8db5faea16123dcb76803c67efe19
thakur Abhishek Singh <thakur.abhisheksinght97@gmail.com>
2025-07-04 08:04:38 +0000

    Google authentication failed on the server.

--- src/main/java/com/example/demo/controller/ApplicantsController.java
+++ src/main/java/com/example/demo/controller/ApplicantsController.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import com.example.demo.model.Applicant;
 import com.example.demo.model.PostOpportunity;
 import com.example.demo.repository.PostOpportunityRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users/application")
 public class ApplicantsController {
--- src/main/java/com/example/demo/controller/CollabrationController.java
+++ src/main/java/com/example/demo/controller/CollabrationController.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +22,7 @@ import com.example.demo.model.User;
 import com.example.demo.repository.CollaborationRepository;
 import com.example.demo.repository.UserRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users/collabration")
 public class CollabrationController {
